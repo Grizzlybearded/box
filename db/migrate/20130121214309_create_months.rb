6882be398cd185a2cadf51834e065b1ce5f19b96
Marcus Gallagher <marcus.gallagher@gmail.com>
2013-02-05 23:09:20 -0500

    heroku: changed migration to eliminate months

--- db/migrate/20130121214309_create_months.rb
+++ db/migrate/20130121214309_create_months.rb
@@ -1,9 +1,6 @@
 class CreateMonths < ActiveRecord::Migration
 
   def change
-    drop_table :months
-    drop_table :monthlies
-
     create_table :months do |t|
       t.date :mend
       t.integer :fund_id
