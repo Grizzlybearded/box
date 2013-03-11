0e0b37de14bcd65f58c3db7980169add9a7bac60
Marcus Gallagher <marcus.gallagher@gmail.com>
2013-03-11 00:32:35 -0400

    momentarily not including any protection around importing data

--- app/models/month.rb
+++ app/models/month.rb
@@ -11,12 +11,12 @@ class Month < ActiveRecord::Base
 
   def self.import(file)
   	CSV.foreach(file.path, headers: true) do |row|
-    	if current_user.investor.funds.where(bmark: false).pluck(:id).include?(row["fund id"])
+    	#if current_user.investor.funds.where(bmark: false).pluck(:id).include?(row["fund id"])
         month = Month.find_by_mend_and_fund_id(Date.strptime(row["mend"],'%b-%y'), row["fund id"]) || 
     			Month.new(mend: Date.strptime(row["mend"],'%b-%y'), fund_id: row["fund id"])
     		month.assign_attributes({fund_return: row["return"]})
     		month.save!
-      end
+      #end
   	end
   end
 end
