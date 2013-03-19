45f56df96224035ad92c592687d0873aa0b83a61
Marcus Gallagher <marcus.gallagher@gmail.com>
2013-03-19 18:44:35 -0400

    email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,21 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Box::Application.initialize!
+
+Box::Application.configure do 
+	# Disable delivery errors, bad email addresses will be ignored
+	config.action_mailer.raise_delivery_errors = true
+
+	# Change mail delivery to either :smtp, :sendmail, :file, :test
+	config.action_mailer.delivery_method = :smtp
+end
+
+ActionMailer::Base.smtp_settings = {
+	:user_name => "app11629203@heroku.com",
+	:password => "goldenshoe14",
+	:domain => "https://nameless-tundra-6004.herokuapp.com",
+	:address => "smtp.sendgrid.net",
+	:port => 465,
+	:authentication => :plain,
+	:enable_starttls_auto => true
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,24 +48,6 @@ Box::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  Box::Application.configure do 
-    # Disable delivery errors, bad email addresses will be ignored
-    config.action_mailer.raise_delivery_errors = true
-
-    # Change mail delivery to either :smtp, :sendmail, :file, :test
-    config.action_mailer.delivery_method = :smtp
-  end
-
-  ActionMailer::Base.smtp_settings = {
-    :user_name => "app11629203@heroku.com",
-    :password => "goldenshoe14",
-    :domain => "https://nameless-tundra-6004.herokuapp.com",
-    :address => "smtp.sendgrid.net",
-    :port => 465,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
-
   # Specify what domain to use for mailer URLs
   #config.action_mailer.default_url_options = {host: "https://nameless-tundra-6004.herokuapp.com"}
 
