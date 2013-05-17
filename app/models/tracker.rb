fd0aec2624f4cdcd1c668f08955350e94716a2d4
Marcus Gallagher <marcus.gallagher@gmail.com>
2013-05-17 01:05:26 -0400

    changed tracker attr_readonly

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -2,6 +2,8 @@ class Tracker < ActiveRecord::Base
   attr_accessible :benchmark_id, :fund_id, :user_id
   attr_readonly :fund_id, :user_id
 
+  #do some sort of validation on which trackers can be added?
+
   belongs_to :fund, class_name: "Fund"
   belongs_to :benchmark, class_name: "Fund"
 
